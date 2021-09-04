09802479d77f0b0f3f4e8b4c18d4696dfa9412de
liuzichao <384914453@qq.com>
2021-09-04 22:11:28 +0800

    demo

--- Web/View/Static/Welcome.hs
+++ Web/View/Static/Welcome.hs
@@ -7,13 +7,13 @@ instance View WelcomeView where
     html WelcomeView = [hsx|
          <div style="background-color: #657b83; padding-top: 2rem; padding-bottom: 2rem; color:hsla(196, 13%, 96%, 1); border-radius: 4px">
               <div style="max-width: 800px; margin-left: auto; margin-right: auto">
-                  <h2 style="margin-top: 0; margin-bottom: 0rem; font-weight: 900; font-size: 3rem">
+                  <h2 style="margin-top: 0; margin-bottom: 0rem; font-weight: 400; font-size: 3rem">
                     刘晓宇的 Design
                   </h2>
               </div>
          </div>
 
-         <div style="max-width: 500px; margin-left: auto; margin-right: auto; margin-top: 4rem">
-              <img src="/aaa.png" style="width:500px">
+         <div style="max-width: 100px; margin-left: auto; margin-right: auto; margin-top: 4rem">
+              <img src="/aaa.png" style="max-width:300px">
          </div> 
 |]
